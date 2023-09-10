e00f2d34d54f1aee6f434cb6f2666608a562c3db
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-09-10 15:37:25 +0600

    general view added

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Sep 2023 09:16:07 GMT -->
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Sales</title>
+
+
+    <link rel="stylesheet" href="{{ asset('backend/css/bootstrap1.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/themefy_icon/themify-icons.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/niceselect/css/nice-select.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/owl_carousel/css/owl.carousel.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/gijgo/gijgo.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/font_awesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/vendors/tagsinput/tagsinput.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/datepicker/date-picker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/vendors/vectormap-home/vectormap-2.0.2.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/scroll/scrollable.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/datatable/css/jquery.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/vendors/datatable/css/responsive.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/vendors/datatable/css/buttons.dataTables.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/text_editor/summernote-bs4.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/morris/morris.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('backend/vendors/material_icon/material-icons.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('backend/css/metisMenu.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('backend/css/style1.css') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/css/colors/default.css') }}" id="colorSkinCSS">
+
+    @yield('css')
+</head>
+
+<body class="crm_body_bg">
+
+
+    @include('backend.layouts.partials._sidebar')
+
+    <section class="main_content dashboard_part large_header_bg">
+
+        @include('backend.layouts.partials._header')
+
+        <div class="main_content_iner overly_inner ">
+            <div class="container-fluid p-0 ">
+
+                @yield('content')
+
+            </div>
+        </div>
+
+        @include('backend.layouts.partials._footer')
+    </section>
+
+    <div id="back-top" style="display: none;">
+        <a title="Go to Top" href="#">
+            <i class="ti-angle-up"></i>
+        </a>
+    </div>
+
+    <script src="{{ asset('backend/js/jquery1-3.4.1.min.js') }}"></script>
+
+    <script src="{{ asset('backend/js/popper1.min.js') }}"></script>
+
+    <script src="{{ asset('backend/js/bootstrap.min.html') }}"></script>
+
+    <script src="{{ asset('backend/js/metisMenu.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/count_up/jquery.waypoints.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/chartlist/Chart.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/count_up/jquery.counterup.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/niceselect/js/jquery.nice-select.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datatable/js/buttons.print.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/datepicker/datepicker.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datepicker/datepicker.en.js') }}"></script>
+    <script src="{{ asset('backend/vendors/datepicker/datepicker.custom.js') }}"></script>
+    <script src="{{ asset('backend/js/chart.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chartjs/roundedBar.min.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/progressbar/jquery.barfiller.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/tagsinput/tagsinput.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/text_editor/summernote-bs4.js') }}"></script>
+    <script src="{{ asset('backend/vendors/am_chart/amcharts.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/scroll/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/scroll/scrollable-custom.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/vectormap-home/vectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/vectormap-home/vectormap-world-mill-en.js') }}"></script>
+
+    <script src="{{ asset('backend/vendors/apex_chart/apex-chart2.js') }}"></script>
+    <script src="{{ asset('backend/vendors/apex_chart/apex_dashboard.js') }}"></script>
+    <script src="{{ asset('backend/vendors/echart/echarts.min.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chart_am/core.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chart_am/charts.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chart_am/animated.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chart_am/kelly.js') }}"></script>
+    <script src="{{ asset('backend/vendors/chart_am/chart-custom.js') }}"></script>
+
+    <script src="{{ asset('backend/js/dashboard_init.js') }}"></script>
+    <script src="{{ asset('backend/js/custom.js') }}"></script>
+
+    @yield('js')
+</body>
+
+
+</html>
