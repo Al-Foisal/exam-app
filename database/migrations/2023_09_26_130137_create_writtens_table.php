274216d759eb0c47d59265c26ee111af25c9f000
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-09-26 16:14:44 +0600

    written db updated

---
+++ database/migrations/2023_09_26_130137_create_writtens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('writtens', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_id');
+            $table->string('topic_id');
+            $table->string('category');
+            $table->string('subcategory');
+            $table->string('childcategory')->nullable();
+            $table->timestamp('published_at');
+            $table->timestamp('expired_at');
+            $table->unsignedBigInteger('duration');
+            $table->string('question_pdf')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('writtens');
+    }
+};
