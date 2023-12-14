d67beea057b582830b34a26d02f3cefeca999665
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-12-14 12:33:10 +0600

    techer fcm update

--- app/Http/Controllers/Api/TeacherPanelController.php
+++ app/Http/Controllers/Api/TeacherPanelController.php
@@ -12,7 +12,7 @@ use App\Models\Written;
 use App\Models\WrittenAnswer;
 use App\Models\WrittenAnswerQuestion;
 use App\Models\WrittenAnswerQuestionScript;
-use App\Service\FCMService;
+use App\Services\FCMService;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
