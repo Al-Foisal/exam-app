994bdbdd90b09bb9fe1543ad17548c9d2a94b49d
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-12-17 13:45:26 +0600

    admin soft delete added

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
 
